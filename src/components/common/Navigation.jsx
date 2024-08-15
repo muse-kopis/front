e78de9cb9527415eaaa09de7cdde1f63f2517e91
rlcz1 <rbf0311@gmail.com>
2024-08-15 19:47:28 +0900

    feat: 하단 네비게이션

---
+++ src/components/common/Navigation.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { URL } from "../../constants/content";
+import { Div } from "./div";
+import { GRAY3 } from "../../constants/color";
+import {  HomeIcon, MypageIcon, SearchIcon, TicketIcon } from "../../assets/icons";
+
+const Container = styled(Div)`
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  width: 100%;
+  max-width: 388px;
+  flex-grow: 1;
+  height: 55px;
+  border-top: solid 1px ${GRAY3};
+  border-radius: 0;
+  justify-content: space-evenly;
+  display: flex;
+  background-color: white;
+`;
+
+const LinkButton = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+
+const Navigation = () => {
+  const location = useLocation();
+
+  const isSelected = (path) => {
+    return location.pathname === path;
+  }
+
+  return (
+    <Container>
+      <LinkButton to={URL.HOME}>
+        <HomeIcon selected={isSelected(URL.HOME)} />
+      </LinkButton>
+      <LinkButton to={URL.SEARCH}>
+        <SearchIcon selected={isSelected(URL.SEARCH)} />
+      </LinkButton>
+      <LinkButton to={URL.TICKET}>
+        <TicketIcon selected={isSelected(URL.TICKET)} />
+      </LinkButton>
+      <LinkButton to={URL.MYPAGE}>
+        <MypageIcon selected={isSelected(URL.MYPAGE)} />
+      </LinkButton>
+    </Container>
+  )
+}
+
+export default Navigation;
