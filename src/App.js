785a67cccdfc79b3cb803f74622b5f2461c5af1b
Gyuri <86146265+rlcz1@users.noreply.github.com>
2024-08-24 22:14:46 +0900

    feat: create ticketbook page (#11)
    * add: create ticket book icons
    
    * feat: create ticket book page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Div } from "./components/common/div";
 import { GRAY3 } from "./constants/color";
-import { Main, Detail, Favorite, Search } from "./pages";
+import { Main, Detail, Favorite, Search, CreateBook } from "./pages";
 
 const App = () => {
   return(
@@ -10,9 +10,10 @@ const App = () => {
       <Div $height='100vh' $margin='0 auto' $border={`1px solid ${GRAY3}`} $overflow='auto'>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/detail/:performancesId" element={<Detail />} />
+          <Route path="/detail/:performanceId" element={<Detail />} />
           <Route path="/favorite" element={<Favorite />} />
           <Route path="/search" element={<Search />} />
+          <Route path="/create-book/:performanceId" element={<CreateBook />} />
         </Routes>
       </Div>
     </BrowserRouter>
