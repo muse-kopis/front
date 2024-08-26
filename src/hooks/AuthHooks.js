7960b26e2906b57fccc2324013fd8882ef4ae9eb
rlcz1 <rbf0311@gmail.com>
2024-08-26 19:00:26 +0900

    auth

--- src/hooks/AuthHooks.js
+++ src/hooks/AuthHooks.js
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom"
 
 export const useAuth = () =>{
-  
+  const navigate = useNavigate();
+
+  console.log('useAuth');
   const handleLogin = () => {
-    
+    navigate('/oauth/redirected/kakao');
   }
 
   return {
