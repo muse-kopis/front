87f9e72c4c9c4791369156cb217c7041181f405b
rlcz1 <rbf0311@gmail.com>
2024-08-30 16:26:16 +0900

    add: search link

--- src/components/pages/favorite/EmptyFavoriteList.jsx
+++ src/components/pages/favorite/EmptyFavoriteList.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { FavoriteIcon } from "../../../assets/icons";
 import { GRAY4, NAVY, WHITE } from "../../../constants/color";
 import { Button, Div, Text } from "../../common/div";
+import { useNavigate } from "react-router-dom";
 
 const ListButton = styled(Button)`
   background-color: ${NAVY};
@@ -15,12 +16,18 @@ const ListButton = styled(Button)`
 `;
 
 const EmptyFavoriteList = () => {
+  const navigate = useNavigate();
+
+  const goList = () => {
+    navigate('/search');
+  }
+
   return (
     <Div $flex={true} $direction='column' $margin='200px 0 0'>
       <FavoriteIcon />
       <Text $weight='BOLD' $margin='26px 0 8px'>저장한 공연이 없어요</Text>
       <Text $size={12} $color={GRAY4}>내 취향인 공연을 찾아 저장해보세요</Text>
-      <ListButton>공연 둘러보러 가기</ListButton>
+      <ListButton onClick={goList}>공연 둘러보러 가기</ListButton>
     </Div>
   )
 }
